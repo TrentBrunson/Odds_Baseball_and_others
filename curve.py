5a32652b97ca78740df0f85288f1e8a1094457cd
TrentBrunson <bbiz2@icloud.com>
2021-10-15 16:02:01 -0500

    distributions

---
+++ curve.py
@@ -0,0 +1,22 @@
+"""
+Read the “Scores.txt” file
+• Calculate descriptive statistics (mean, standard deviation, number of scores, low score,
+and high score) – NOTE: While there are several libraries available with functions to 
+Last modified: September 28, 2021 Page 3 of 6
+calculate descriptive statistics, I am requiring you to write your own code to do these
+calculations (see Figure 1 for standard deviation formula).
+• Display the descriptive statistics in a nicely formatted table (see Figure 2) – (NOTE: For
+the formatted table, you should round the average and standard deviation values for
+DISPLAY PURPOSES ONLY.)
+• Use the grade curve table to determine the letter grade for each numeric score (NOTE:
+Use the unrounded average and standard deviation values to calculate the cutoff
+points. Round the cutoff points to 1 place after the decimal when determining letter
+grades.)
+• Create a new file named “Scores-and-Letter-Grades.txt” that contains the score and
+letter grade on the same line (see sample first lines of new file in Figure 3)
+• Count the total number of scores for each letter grade (i.e., number of As, Bs, Cs, etc.) –
+HINT: A dictionary is an efficient tool for accumulating counts.
+• Display a curved grade cut-off-point table (see Figure 2)
+• Display a grade distribution table (see Figure 2)
+"""
+#%%
