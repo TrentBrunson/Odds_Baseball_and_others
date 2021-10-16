5b321b219bc10baad70fddcaf54a3fe63557808e
TrentBrunson <bbiz2@icloud.com>
2021-10-15 19:19:36 -0500

    output file updated

--- curve.py
+++ curve.py
@@ -1,23 +1,7 @@
 """
 Read the “Scores.txt” file
-• Calculate descriptive statistics (mean, standard deviation, number of scores, low score,
-and high score) – NOTE: While there are several libraries available with functions to 
-Last modified: September 28, 2021 Page 3 of 6
-calculate descriptive statistics, I am requiring you to write your own code to do these
-calculations (see Figure 1 for standard deviation formula).
-• Display the descriptive statistics in a nicely formatted table (see Figure 2) – (NOTE: For
-the formatted table, you should round the average and standard deviation values for
-DISPLAY PURPOSES ONLY.)
-• Use the grade curve table to determine the letter grade for each numeric score (NOTE:
-Use the unrounded average and standard deviation values to calculate the cutoff
-points. Round the cutoff points to 1 place after the decimal when determining letter
-grades.)
-• Create a new file named “Scores-and-Letter-Grades.txt” that contains the score and
-letter grade on the same line (see sample first lines of new file in Figure 3)
-• Count the total number of scores for each letter grade (i.e., number of As, Bs, Cs, etc.) –
-HINT: A dictionary is an efficient tool for accumulating counts.
-• Display a curved grade cut-off-point table (see Figure 2)
-• Display a grade distribution table (see Figure 2)
+• Calculate descriptive statistics (mean, standard deviation, number of scores, 
+low score, and high score) 
 """
 #%%
 import csv
@@ -41,7 +25,6 @@ mid = int(round(len(scoreList)/2, 0))
 
 if counter % 2 == 0:
     median = (scoreList[mid] + scoreList[mid+1])/2
-    print(scoreList[mid], mid, scoreList[mid+1])
 else:
     median = scoreList[mid+1]
 
@@ -89,7 +72,7 @@ gradeDict.update({'B':countB})
 gradeDict.update({'C':countC})
 gradeDict.update({'D':countD})
 gradeDict.update({'F':countF})
-print(gradeDict)
+
 with open("data/Scores-and-Letter-Grades.txt", "w", newline='') as file:
     csv.writer(file, delimiter=",").writerows(newList)
     # file.writelines("%s\n" % item for item in newList)
@@ -114,10 +97,4 @@ print(
     f"GRADE DISTRIBUTION AFTER CURVING GRADES USING CUT \nOFF POINTS FROM ABOVE\n"
     f"A:  {gradeDict['A']}\tB:  {gradeDict['B']}\tC:  {gradeDict['C']}\tD:  {gradeDict['D']}\tF:  {gradeDict['F']}\t"
 )
-# %%
-print(*newList)
-# %%
-a = ['a,','65,']
-str(a).strip(",")
-print(*a)
 # %%
