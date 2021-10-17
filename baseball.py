cb3e665e98cd2883baacb747573a795a5beaef85
TrentBrunson <bbiz2@icloud.com>
2021-10-17 13:33:58 -0500

    baseball

---
+++ baseball.py
@@ -0,0 +1,12 @@
+"""
+Write a program to calculate the
+percentage of games won by each team. Create a new data file named “mlb-division-standings2019.csv” that contains all the original data plus the percentage of games won. You should sort
+the data in the new file first by division (ascending order) and second by percentage of games
+won (descending order).
+Display the division standings to the user. Each division should be listed separate from the other
+divisions. Your standings should include rank (1-5), team, wins, losses, and win % for each team
+under a division header. You should round the percentage of games won to two places after the
+decimal."""
+#%%
+import pandas as pd
+# %%
