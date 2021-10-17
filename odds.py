493a6deed79fa0a90651594e6e60dc603fe1cfbe
TrentBrunson <bbiz2@icloud.com>
2021-10-17 17:04:37 -0500

    odds plots

--- odds.py
+++ odds.py
@@ -2,8 +2,10 @@
 #It shows output of Doubles six probability and any other Doubles for throwing paired die.
 #Program output shows 10000 rounds  and each round has 24 Rolls of dices random output finding double sixes and double anyother same number for both die 
 
-
 #%%
+from numpy.core.fromnumeric import nonzero
+
+
 def rolldice(): #This function gets output for  4-c
     import random
     dieOne = 0
@@ -12,23 +14,21 @@ def rolldice(): #This function gets output for  4-c
     totalDoubleSix = 0
     six = 6
     line=""
-    doubles=0
+    # doubles=0
     one = 1
     zero = 0
     listc = []
     lofl = []
-    glist = []
+    # glist = []
     gline=""
-    #import csv
-    #with open("round.csv", "w", newline="\n") as file:
-        #writer = csv.writer(file)
+
     with open("round.txt", "w") as wfile:
         with open("gamblehouse.txt", "w") as gfile:        
             for i in range(1,10001):
                 line=""
                 listc = []
                 gline=""
-                glist = []
+                # glist = []
                 doubleSix = 0
                 gline = str(i) + "," 
                 for dice in range(1,25):
@@ -36,7 +36,6 @@ def rolldice(): #This function gets output for  4-c
                     dieOne = random.randint(1, 6)
                     dieTwo = random.randint(1, 6)
                     line = str(i) + "," + str(dice) + "," + str(dieOne) + "," + str(dieTwo) + "," 
-               
                     if dieOne == dieTwo: # finds both die has same number
                         line = line + str(one) + ","
                     else:
@@ -48,33 +47,16 @@ def rolldice(): #This function gets output for  4-c
                         totalDoubleSix += 1
                     else:
                         line = line + str(zero)
-
-                                   
-                #print(line)
                     listc.append(line)
                     wfile.write(f"{line}\n")
-
-                #writer.writerow(line)
-
                 if doubleSix >= 1:
                     gline = str(i) + "," + "True" + "," + "Gambler" + "," + str(doubleSix)
                 else:
                     gline = str(i) + "," + "False" + "," + "House" + "," + str(zero)
-                
-            #print(gline)
-            
                 gfile.write(f"{gline}\n")
-                
-            lofl.append(listc)    
-        #print(lofl)
-        
+            lofl.append(listc)            
         gfile.close()
-    wfile.close()
-        
-                
-        
-            
-        #return lofl        
+    wfile.close()       
 
 def gamblehouse(): #This function gets output for  4-d
     import random
@@ -82,9 +64,9 @@ def gamblehouse(): #This function gets output for  4-d
     dieTwo = 0
     six = 6
     line=""
-    doubles=0
-    one = 1
-    zero = 0
+    # doubles=0
+    # one = 1
+    # zero = 0
     listc = []
     lofl = []
     nodoubleSix = (35/36) * 24
@@ -117,14 +99,10 @@ def gamblehouse(): #This function gets output for  4-d
                 else:
                     line = line + "House"
                     probofDoublesix = 0
-                    
-                #print(line)
                 listc.append(line)
                 line = line + "," + str(totalDoubleSix)
-                wfile.write(f"{line}\n")
-                #writer.writerow(line)
-            
-            probofDoublesix = 1 - nodoubleSix
+                wfile.write(f"{line}\n")            
+            # probofDoublesix = 1 - nodoubleSix
             lofl.append(listc)    
     
 
@@ -142,11 +120,103 @@ def main():
     rolldice()
     #gamblehouse()
     #displayone(cdata)()
-    
-
-
 
 if __name__ == "__main__":
     main()
+# %%
+def rolldice(): #This function gets output for  4-c
+    import random
+    dieOne = 0
+    dieTwo = 0
+    doubleSix = 0
+    totalDoubleSix = 0
+    six = 6
+    line=""
+    # doubles=0
+    one = 1
+    zero = 0
+    listc = []
+    lofl = []
+    # glist = []
+    gline=""
+
+    with open("roll.csv", "w") as wfile:
+        with open("gambler.csv", "w") as gfile:        
+            for i in range(1,10001):
+                line=""
+                listc = []
+                gline=""
+                doubleSix = 0
+                gline = str(i) + "," 
+                for dice in range(1,25):
+                    line=""
+                    dieOne = random.randint(1, 6)
+                    dieTwo = random.randint(1, 6)
+                    line = str(i) + "," + str(dice) + "," + str(dieOne) + "," + str(dieTwo) + "," 
+                    if dieOne == dieTwo: # finds both die has same number
+                        line = line + str(one) + ","
+                    else:
+                        line = line + str(zero) + ","
+
+                    if dieOne == six and dieTwo == six: # finds both die has number six
+                        line = line + str(one) 
+                        doubleSix += 1
+                        totalDoubleSix += 1
+                    else:
+                        line = line + str(zero)
+                    listc.append(line)
+                    wfile.write(f"{line}\n")
+                if doubleSix >= 1:
+                    gline = str(i) + "," + "True" + "," + "Gambler" + "," + str(doubleSix)
+                else:
+                    gline = str(i) + "," + "False" + "," + "House" + "," + str(zero)
+                gfile.write(f"{gline}\n")
+            lofl.append(listc)            
+        gfile.close()
+    wfile.close()
+    # return lofl     
+
+def analytics():
+    import pandas as pd
+    gamblerDF = pd.read_csv('gambler.csv')
+    rollDF = pd.read_csv('roll.csv', names=(
+        'round_num', 'roll_num', 'die1', 'die2', 'doubles', 'double_six'))
+    print(rollDF)
+    print("Index: ", rollDF.index)
+    print("Columns: ", rollDF.columns)
+    print("Size: ", rollDF.size)
+    print("Shape: ", rollDF.shape)
+    print("Values: ", rollDF.values)
+    print("Info: ", rollDF.info())
+    rollDF.info()
+    double6 = rollDF['double_six'].sum()
+    doubles = rollDF['doubles'].sum()
+    print(
+        f"You just ran a set of 24 rolls, 10,000 times for a total of 240,000 rolls.\n"
+        f"You rolled doubles {doubles:,} times, or {doubles/240000:.2%} of the time.\n"
+        f"You rolled double sixes {double6:,} times, or {double6/240000:.2%} of the time.\n\n"
+        f"In 24 rolls, you typically see double sixes about 49% of the time.\n"
+        f"In your 10,000 sets of 24 dice rolls you rolled double sixes {double6/10000:.2%} of the time.\n"    
+        )
+    if (double6/10000) > .49:
+        print("Book your flight to Vegas, baby!\n\n")
+    elif (double6/10000) > .4:
+        print("Your results are in the range of what is expected.\n\n")
+    else:
+        print("Better luck next time.\n\n")
+    rollDF.groupby('die1')['die1'].agg(['mean','median','std']).plot.bar()
+
+    import seaborn as sns
+    sns.histplot(rollDF['die1'], bins = 6, legend=False)
+    rollDF["die1"].plot()
+    return
+
+def main():
+    rolldice()
+    analytics()
+
+if __name__ == "__main__":
+    main()
+# %%
 
 # %%
